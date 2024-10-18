2c2098a84a81618b47a8c445f027945cb409f68b
Nosferatu <nosferatudima@gmail.com>
2024-10-18 16:40:40 +0300

    Новая версия

--- main.cpp
+++ main.cpp
@@ -2,15 +2,35 @@
 #include <fstream>
 #include <string>
 #include <set>
+#include <vector>
+#include <sstream>
 
 using namespace std;
+
+void LoginFailureAddToSet (string line, std::set<string> &IpBlockStrings)
+{
+    stringstream ss(line);
+    string token;
+    vector<string> tokens;
+    char delimiter = ' ';
+
+    while (getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    if( tokens[3] == "login" && tokens[4] == "failure" )
+    {
+        IpBlockStrings.insert("/ip firewall address-list add address="+ tokens[9] +" list=IPBlock");
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
    // system("chcp 1251");
    // system("chcp 866");
     system("cls");
-// test
+// testh
     std::set<string> IpBlockStrings;
 
     if (argc >= 2)
@@ -23,26 +43,7 @@ int main(int argc, char *argv[])
             std::string line;
             while (std::getline(fout, line))
             {
-
-                if (line.find("login failure") != std::string::npos)
-                {
-
-                   // std::cout << line.find("login failure") << std::endl;
-                    int a = 0, b = 0;
-                    for(unsigned int i = line.find("login failure"), sc=0; i < line.length(); i++)
-
-                    {
-                        if (line[i] == ' ')sc++;
-                        if (sc == 6 && a == 0) a = i;
-                        if (sc == 7)
-                        {
-                            b = i;
-                            break;
-                        }
-                    }
-
-                    IpBlockStrings.insert("/ip firewall address-list add address="+ line.substr((a+1),(b - a)) +"list=IPBlock");
-                }
+                LoginFailureAddToSet(line, IpBlockStrings);
             }
         }
 
